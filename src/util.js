2cdda5d8db6637ecde224754359cbf9bcddc9933
Manish Pathak <immanish8521@gmail.com>
2021-03-01 22:46:19 +0530

    bug-fix/type-error

--- src/util.js
+++ src/util.js
@@ -1,11 +1,18 @@
 //MEdia resize
 
 export const smallImage = (imagePath, size) => {
-  const image = imagePath.match(/media\/screenshots/)
-    ? imagePath.replace(
-        "media/screenshots",
-        `media/resize/${size}/-/screenshots`
-      )
-    : imagePath.replace("/media/games/", `/media/resize/${size}/-/games/`);
-  return image;
+ let image;
+  if (imagePath) {
+    image = imagePath.match(/media\/screenshots/)
+      ? imagePath.replace(
+          "media/screenshots",
+          `media/resize/${size}/-/screenshots`
+        )
+      : imagePath.replace("/media/games", `/media/resize/${size}/-/games`);
+  } else {
+    image = imagePath;
+  }
+return image;
 };
+
+ 
